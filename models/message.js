efafd20153d07ccf38399ce729ad3200aa87b81f
CHRIS BARNARD <barnard.chris@gmail.com>
2015-02-24 00:21:47 -0600

    make structure change of message schema

--- models/message.js
+++ models/message.js
@@ -6,21 +6,23 @@ var MessageSchema = new mongoose.Schema(
   // sharing controls who can see the message
   // publicShare messages can be seen by anyone who follows your account
   // privateShare messages are only visible by a specified list of accounts
-  , sharing : { public : { type: Boolean, default : true } 
+  , sharing : { publicFlag : { type: Boolean, default : true } 
               , privateList : Array
               }
-  // videoUrl and audioUrl are options of where to store the source file
-  , videoUrl : { type : String, default : null }
-  , audioUrl : { type : String, default : null }
-  // options to track upload and merging progress 
-  , compressed : { type : Boolean, default : false }
-  , audioVideoMerged : { type : Boolean, default : false }
-  , completedFileUrl : { type : String, default : null }
-  // text message caption
-  , caption : { type : String, default : null }
+  , body : { mediaUrl : { type : String, default : null }
+           // text message caption
+           , caption : { type : String, default : null }
+           }
   // an array of comments
   , comments : Array
   , dateReceived : { type : Date, default : Date.now }
+  /*
+  // options to track upload and merging progress 
+  , compressed : { type : Boolean, default : false }
+  , audioVideoMerged : { type : Boolean, default : false }
+  // videoUrl and audioUrl are options of where to store the source file
+  , videoUrl : { type : String, default : null }
+  , audioUrl : { type : String, default : null }*/
   }
 )
 
